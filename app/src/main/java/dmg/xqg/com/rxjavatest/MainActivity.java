544ede7ad703a7693de7974fa9a46218509552a4
zhenxing <zhenxing@2016>
2018-01-08 15:10:24 +0800

    code

--- app/src/main/java/dmg/xqg/com/rxjavatest/MainActivity.java
+++ app/src/main/java/dmg/xqg/com/rxjavatest/MainActivity.java
@@ -1,9 +1,13 @@
 package dmg.xqg.com.rxjavatest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import dmg.xqg.com.rxjavatest.ui.ShowPicActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -29,9 +33,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.tvShowPic){
-
+            startActivity(new Intent(MainActivity.this,ShowPicActivity.class));
         }else if (1 == 2){
-
+            Log.e("chris","chris");
         }
     }
 }
--- app/src/main/java/dmg/xqg/com/rxjavatest/ui/ShowPicActivity.java
+++ app/src/main/java/dmg/xqg/com/rxjavatest/ui/ShowPicActivity.java
@@ -1,11 +1,19 @@
 package dmg.xqg.com.rxjavatest.ui;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
 
 import dmg.xqg.com.rxjavatest.R;
 import rx.Observable;
@@ -16,9 +24,9 @@ import rx.schedulers.Schedulers;
 
 public class ShowPicActivity extends AppCompatActivity {
 
-    private ImageView iv01;
-    private ImageView iv02;
-    private ImageView iv03;
+    private File[] folders;
+    private ArrayList<Bitmap> bitmaps;
+    private LinearLayout llRoot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +40,37 @@ public class ShowPicActivity extends AppCompatActivity {
     }
 
     private void initData() {
-        File[] folders = new File[]{
+        InputStream aInputStream = getResources().openRawResource(R.raw.a);
+        File aFile = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), "a.png");
+        InputStream bInputStream = getResources().openRawResource(R.raw.b);
+        File bFile = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), "b.png");
+        InputStream cInputStream = getResources().openRawResource(R.raw.c);
+        File cFile = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), "c.png");
 
+        getFileFromStream(aInputStream, aFile);
+        getFileFromStream(bInputStream, bFile);
+        getFileFromStream(cInputStream, cFile);
+
+        folders = new File[]{
+                aFile,
+                bFile,
+                cFile
         };
+    }
+
+    private void getFileFromStream(InputStream inputStream, File file) {
+        try {
+            OutputStream os = new FileOutputStream(file);
+            int bytesRead = 0;
+            byte[] buffer = new byte[8192];
+            while ((bytesRead = inputStream.read(buffer, 0, 8192)) != -1) {
+                os.write(buffer, 0, bytesRead);
+            }
+            os.close();
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void initListener() {
@@ -42,13 +78,14 @@ public class ShowPicActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        iv01 = (ImageView) findViewById(R.id.iv01);
-        iv02 = (ImageView) findViewById(R.id.iv02);
-        iv03 = (ImageView) findViewById(R.id.iv03);
+        llRoot = (LinearLayout) findViewById(R.id.llRoot);
     }
 
     private void show() {
-        Observable.from(folders)
+        Observable
+                // 数据源
+                .from(folders)
+                //
                 .flatMap(new Func1<File, Observable<File>>() {
                     @Override
                     public Observable<File> call(File file) {
@@ -77,12 +114,38 @@ public class ShowPicActivity extends AppCompatActivity {
                 .subscribe(new Action1<Bitmap>() {
                     @Override
                     public void call(Bitmap bitmap) {
-                        imageCollectorView.addImage(bitmap);
+                        addAndShowImage(bitmap);
                     }
                 });
     }
 
+    /**
+     * 添加图片并显示
+     */
+    private void addAndShowImage(Bitmap bitmap) {
+        bitmaps = new ArrayList<>();
+        bitmaps.add(bitmap);
+
+        // 显示所有图片
+        for (int i = 0; i < bitmaps.size(); i++) {
+            ImageView iv = new ImageView(ShowPicActivity.this);
+            iv.setImageBitmap(bitmaps.get(i));
+            llRoot.addView(iv);
+        }
+    }
+
     private Bitmap getBitmapFromFile(File file) {
-        return null;
+        return BitmapFactory.decodeFile(file.getAbsolutePath(),getBitmapOption(2));
+    }
+
+    /**
+     * 采样率设置为2，避免内存溢出
+     */
+    private BitmapFactory.Options getBitmapOption(int inSampleSize){
+        System.gc();
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inPurgeable = true;
+        options.inSampleSize = inSampleSize;
+        return options;
     }
 }
